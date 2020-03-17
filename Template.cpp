e75ab22f7a9279f45e35b590447b6140dd54ae95
Shubham Madankar <sgmadankar@gmail.com>
2020-03-17 11:04:01 +0530

    Added Cpp Template File

---
+++ Template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vvll = vector< vector<ll> >;
+using vll = vector<ll> ;
+using pll = pair<ll, ll> ;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define fastio(); ios_base::sync_with_stdio(false); cin.tie(NULL); 
+
+
+const int maxS = 1e5 + 7;
+
+
+int main() {
+    fastio();
+    
+}
